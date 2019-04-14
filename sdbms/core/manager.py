f8d79282953308078f25f93aa2227b22baed7c66
Samarithan <alexandru.vasiliu2017@gmail.com>
2019-04-14 03:48:12 +0300

    implement insert & add columns functionality

--- sdbms/core/manager.py
+++ sdbms/core/manager.py
@@ -6,28 +6,29 @@ SCHEMA = '.schema'
 
 class DbManager(object):
     def __init__(self, db_name, root_path):
-        self.db_path = os.path.join(self.root_path, self.db_path)
+        self.db_path = os.path.join(root_path, db_name)
 
     def _put_schema(self, schema, schema_path):
         with open(schema_path, 'w') as fd:
             for col_name, col_type in schema.items():
-                fd.write(f'{col_name},{col_type}')
+                fd.write(f'{col_name},{col_type}\n')
     
     def _get_schema(self, schema_path):
         schema = {}
         with open(schema_path, 'r') as fd:
             while True:
-                line = fd.readline()
+                line = fd.readline().strip('\n')
+                print(line)
                 if not line:
-                    return
+                    break
                 col_name, col_type = line.split(',')
                 schema[col_name] = col_type
         return schema
 
-    def create_db(self, name):
+    def create_db(self):
         os.mkdir(self.db_path)
 
-    def delete_db(self, name):
+    def delete_db(self):
         shutil.rmtree(self.db_path)
     
     def create_table(self, name, schema={}):
@@ -43,8 +44,21 @@ class DbManager(object):
 
     def add_column(self, name, col_name, col_type):
         # modify schema (add new col)
+        # name means table name
+        table_path = os.path.join(self.db_path,name)
+        schema_path = os.path.join(table_path,SCHEMA)
+        with open(schema_path,'a') as fd:
+            fd.write(f'{col_name},{col_type}\n')
+        
         # go into each record
             # create another empty column
+        for root,dirs,files in os.walk(table_path): # rework needed
+            for current_row in dirs:
+                column_name = col_name +"."+col_type
+                row_path = os.path.join(table_path,current_row)
+                column_path = os.path.join(row_path,column_name)
+                new_column = open(column_path,"w")
+                new_column.close()
         pass
     
     def del_column(self, name, col_name):
@@ -56,14 +70,33 @@ class DbManager(object):
     def insert_row(self, table, row={}):
         # get table path
         # get schema
+        # table means table_name
+        table_path = os.path.join(self.db_path,table)
+        schema_path = os.path.join(table_path,SCHEMA)
+        current_schema = self._get_schema(schema_path)
 
         # all records dirs names are number (e.g. 1, 2, 3...)
+        subdirs = [os.path.join(table_path, obj) for obj in os.listdir(table_path) if os.path.isdir(os.path.join(table_path,obj))]
+        if len(subdirs) == 0:
+            new_index = 0
+        else:
+            new_index = len(subdirs)
+
+        row_path = os.path.join(table_path,str(new_index))
+
         # get the maximum record name and + 1 on it (autoincrement)
             # if no records found, default to 0
         # create a record dir with the new name
-
+        os.mkdir(row_path)
         # for each column in schema, create a file (e.g. name.str)
         # put the data in it (e.g. from row['name'] in name.str)
+
+        for col_name, col_type in current_schema.items():
+            fileName = col_name + "."+col_type
+            filePath = os.path.join(row_path,fileName)
+            fd = open(filePath,"w")
+            fd.write(row[col_name])
+            fd.close()
         pass
     
     def scan_rows(self, table):
