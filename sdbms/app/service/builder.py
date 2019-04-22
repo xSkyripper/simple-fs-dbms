5a9376481846351301a593a3ac0e617252c6b710
cernescustefan <stefan.cernescu94@gmail.com>
2019-04-22 14:12:24 +0300

    added CRUD operations

---
+++ sdbms/app/service/builder.py
@@ -0,0 +1,145 @@
+class QueryBuilder(object):
+    def __init__(self):
+        pass
+
+    def build_select(self,*args):
+        table_name = args[0]['TableName']
+        condition_type = args[0]['conditionType']
+        counter = 1
+        keys = list()
+        operators = list()
+        values = list()
+        for element in args[0]:
+            if counter>=4:
+                if counter %3 == 1:
+                    keys.append(args[0][element])
+                if counter %3 == 2:
+                    operators.append(args[0][element])
+                if counter %3 == 0:
+                    try:
+                        value = int(args[0][element])
+                    except ValueError:
+                        if args[0][element] == 'False' or args[0][element] == 'True':
+                            value = args[0][element]
+                        else:
+                            value = "\"" + args[0][element] + "\""
+                    values.append(str(value))
+            counter+=1
+        query = "query * " + table_name
+        if len(condition_type) >0:
+            query+= " where op:"+condition_type+" conditions"
+            for i in range(len(keys)):
+                query+= " "+keys[i] + operators[i] + values[i]
+        query+= ";"
+        return query
+
+    def build_insert(self,*args):
+        table_name = args[0]['TableName']
+        keys = list()
+        values = list()
+        query = "insert into " + table_name + " values "
+        counter = 1
+        for element in args[0]:
+            if counter>=3:
+                if counter %2 == 1:
+                    keys.append(args[0][element])
+                if counter %2 == 0:
+                    try:
+                        value = int(args[0][element])
+                    except ValueError:
+                        if args[0][element] == 'False' or args[0][element] == 'True':
+                            value = args[0][element]
+                        else:
+                            value = "\"" + args[0][element] + "\""
+                    values.append(str(value))
+            counter+=1
+        for i in range(len(keys)):
+            query += " " + keys[i] + "=" + values[i]
+        query += ";"
+        return query
+
+    def build_delete(self, *args):
+        table_name = args[0]['TableName']
+        condition_type = args[0]['conditionType']
+        counter = 1
+        keys = list()
+        operators = list()
+        values = list()
+        for element in args[0]:
+            if counter >= 4:
+                if counter % 3 == 1:
+                    keys.append(args[0][element])
+                if counter % 3 == 2:
+                    operators.append(args[0][element])
+                if counter % 3 == 0:
+                    try:
+                        value = int(args[0][element])
+                    except ValueError:
+                        if args[0][element] == 'False' or args[0][element] == 'True':
+                            value = args[0][element]
+                        else:
+                            value = "\"" + args[0][element] + "\""
+                    values.append(str(value))
+            counter += 1
+        query = "delete in " + table_name
+        if len(condition_type) > 0:
+            query += " where op:" + condition_type + " conditions"
+            for i in range(len(keys)):
+                query += " " + keys[i] + operators[i] + values[i]
+        query += ";"
+        return query
+
+    def build_update(self, *args):
+        table_name = args[0]['TableName']
+        condition_type = args[0]['conditionType']
+        labels = list()
+        labelValues = list()
+        keys = list()
+        operators = list()
+        values = list()
+        counter = 1
+        for element in args[0]:
+            if "myLabel" in element:
+                if counter % 2 == 1:
+                    labels.append(args[0][element])
+                else:
+                    try:
+                        value = int(args[0][element])
+                    except ValueError:
+                        if args[0][element] == 'False' or args[0][element] == 'True':
+                            value = args[0][element]
+                        else:
+                            value = "\"" + args[0][element] + "\""
+                    labelValues.append(value)
+            counter+=1
+        counter = 1
+        for element in args[0]:
+            if counter >= 6:
+                if counter % 3 == 0:
+                    keys.append(args[0][element])
+                if counter % 3 == 1:
+                    operators.append(args[0][element])
+                if counter % 3 == 2:
+                    try:
+                        value = int(args[0][element])
+                    except ValueError:
+                        if args[0][element] == 'False' or args[0][element] == 'True':
+                            value = args[0][element]
+                        else:
+                            value = "\"" + args[0][element] + "\""
+                    values.append(str(value))
+            counter += 1
+        query = "update "+ table_name + " set "
+        for i in range(len(labels)):
+            query+= labels[i]+"="+str(labelValues[i]) + " "
+        if len(condition_type) >0:
+            query+= " where op:"+condition_type+" conditions"
+            for i in range(len(keys)):
+                query+= " "+keys[i] + operators[i] + values[i]
+        query+= ";"
+        return query
+
+    def use_db(self, *args):
+        database_name = args[0]['DbName']
+        query = "use sdb " + database_name +";"
+        return query
--- sdbms/app/views.py
+++ sdbms/app/views.py
@@ -1,14 +1,97 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+from sdbms.app.service.builder import QueryBuilder
+from sdbms.core.manager import DbManager
+from sdbms.core.parser import QueryParser, CommandError
 
+root_path = "/Users/cernescustefan/Documents/Facultate/db"
 
 main_api = Blueprint('main', __name__,
                      template_folder='templates')
 
+
 @main_api.route('/', methods=['GET'])
 def index():
-    context = {
-        'some_list': [1, 2, 3, 4, 5],
-        'some_string': 'Hello world',
-        'some_int': 12345,
-    }
-    return render_template('index.html', context=context)
+    return render_template('index.html')
+
+@main_api.route('/select', methods=['GET'])
+def select():
+    return render_template('select.html')
+
+@main_api.route('/insert', methods=['GET'])
+def insert():
+    return render_template('insert.html')
+
+@main_api.route('/delete', methods=['GET'])
+def delete():
+    return render_template('delete.html')
+
+@main_api.route('/update', methods=['GET'])
+def update():
+    return render_template('update.html')
+
+
+@main_api.route('/result', methods=['POST', 'GET'])
+def result():
+    if request.method == 'POST':
+        result = request.form
+        queryBuilder = QueryBuilder()
+        set_db = queryBuilder.use_db(result)
+        query = queryBuilder.build_select(result)
+        db_manager = DbManager(root_path)
+        parser = QueryParser()
+        cmd = parser.parse(set_db)
+        rv = cmd.execute(db_manager)
+        cmd = parser.parse(query)
+        rv = cmd.execute(db_manager)
+
+    context = list(rv)
+    return render_template('result.html', context=context)
+
+@main_api.route('/insertResult', methods=['POST', 'GET'])
+def insertResult():
+    if request.method == 'POST':
+        result = request.form
+        queryBuilder = QueryBuilder()
+        set_db = queryBuilder.use_db(result)
+        query = queryBuilder.build_insert(result)
+        db_manager = DbManager(root_path)
+        parser = QueryParser()
+        cmd = parser.parse(set_db)
+        rv = cmd.execute(db_manager)
+        cmd = parser.parse(query)
+        rv = cmd.execute(db_manager)
+
+    return render_template('insertResutl.html')
+
+
+@main_api.route('/deleteResult', methods=['POST', 'GET'])
+def deleteResult():
+    if request.method == 'POST':
+        result = request.form
+        queryBuilder = QueryBuilder()
+        set_db = queryBuilder.use_db(result)
+        query = queryBuilder.build_delete(result)
+        db_manager = DbManager(root_path)
+        parser = QueryParser()
+        cmd = parser.parse(set_db)
+        rv = cmd.execute(db_manager)
+        cmd = parser.parse(query)
+        rv = cmd.execute(db_manager)
+
+    return render_template('deleteResult.html')
+
+@main_api.route('/updateResult', methods=['POST', 'GET'])
+def updateResult():
+    if request.method == 'POST':
+        result = request.form
+        queryBuilder = QueryBuilder()
+        set_db = queryBuilder.use_db(result)
+        query = queryBuilder.build_update(result)
+        db_manager = DbManager(root_path)
+        parser = QueryParser()
+        cmd = parser.parse(set_db)
+        rv = cmd.execute(db_manager)
+        cmd = parser.parse(query)
+        rv = cmd.execute(db_manager)
+
+    return render_template('updateResult.html')
